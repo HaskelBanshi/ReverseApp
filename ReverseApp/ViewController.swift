a86dfa25e0304e7cfa135fdbeb0fc89686eec003
author <wifi.oleg.london@gmail.com>
2022-08-24 17:41:57 +0300

    First

--- ReverseApp/ViewController.swift
+++ ReverseApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+                view.addGestureRecognizer(tap)
+            }
+    
+
+    
+    @IBAction func Button(_ sender: Any) {
+        let str = textField.text!
+        var result = ""
+        str.enumerateSubstrings(in: str.startIndex..., options: .byWords) { string, range, enclosingRange, _ in
+            result.append(contentsOf: string!.reversed())
+            result.append(contentsOf: str[range.upperBound..<enclosingRange.upperBound])
+        }
+        label.text = result
     }
-
-
+    
+@objc func dismissKeyboard() {
+      view.endEditing(true)
+    }
 }
-
