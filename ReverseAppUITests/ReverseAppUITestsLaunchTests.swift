8c23fc6c2535f8ed1ae04a0714533d11a4a438ce
author <wifi.oleg.london@gmail.com>
2022-08-24 15:13:07 +0300

    Initial Commit

---
+++ ReverseAppUITests/ReverseAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ReverseAppUITestsLaunchTests.swift
+//  ReverseAppUITests
+//
+//  Created by a2 on 24.08.2022.
+//
+
+import XCTest
+
+class ReverseAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
